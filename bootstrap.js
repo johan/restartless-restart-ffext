0613cf743b51559806949ab748b83597140ed1a9
Erik Vold <erikvvold@gmail.com>
2010-11-02 14:13:55 -0700

    adding version 1 of Restartless Restart

---
+++ bootstrap.js
@@ -0,0 +1,48 @@
+const {classes: Cc, interfaces: Ci, utils: Cu} = Components;
+Cu.import("resource://gre/modules/Services.jsm");
+
+let cleanupAry = [];
+
+function restart() {
+  Cc['@mozilla.org/toolkit/app-startup;1'].getService(Ci.nsIAppStartup)
+      .quit(Ci.nsIAppStartup.eAttemptQuit | Ci.nsIAppStartup.eRestart);
+}
+
+function main(win) {
+  let doc = win.document;
+  let restartMI = doc.createElement("menuitem");
+  let fileMenu = doc.getElementById("menu_FilePopup");
+  let quitMI = doc.getElementById("menu_FileQuitItem");
+  restartMI.setAttribute("id", "menu_FileRestartItem");
+  restartMI.setAttribute("label", "Restart");
+  restartMI.addEventListener("command", restart, true);
+  fileMenu.insertBefore(restartMI, quitMI);
+
+  let len = cleanupAry.push(function() fileMenu.removeChild(restartMI));
+  win.addEventListener("unload", function() {
+    cleanupAry[len - 1] = null;
+  }, false);
+}
+
+function findWindowsAndRun(aFunc) {
+  let browserWins = Services.wm.getEnumerator("navigator:browser");
+  while (browserWins.hasMoreElements()) aFunc(browserWins.getNext());
+
+  function winObs(aSubject, aTopic) {
+    if ("domwindowopened" != aTopic) return;
+    let winLoad = function() {
+      aSubject.removeEventListener("load", winLoad, false);
+      if ("navigator:browser" ==
+          aSubject.document.documentElement.getAttribute("windowtype"))
+        aFunc(aSubject);
+    }
+    aSubject.addEventListener("load", winLoad, false);
+  }
+  Services.ww.registerNotification(winObs);
+  cleanupAry.push(function() Services.ww.unregisterNotification(winObs));
+}
+
+function startup() findWindowsAndRun(main);
+function shutdown() {
+  for (let [, cleaner] in Iterator(cleanupAry)) cleaner && cleaner();
+}
