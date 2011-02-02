e3665e6c7f6421a20577a91d10f39ea68b8174e2
Erik Vold <erikvvold@gmail.com>
2011-02-01 23:18:31 -0800

    less is more

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 
 VER=`grep -Go 'version\>\(.*\)\<' src/install.rdf | grep -Go '>\(.*\)<' | sed -e 's/[><]*//g'`
 XPI="restartless-restart-$VER.xpi"
+echo "Building $XPI ..."
 
 # Copy base structure to a temporary build directory and move in to it
-echo "Creating build directory ..."
 cd src
 rm -rf build
 mkdir build
@@ -13,16 +13,14 @@ cp -r \
   build/
 cd build
 
-echo "Cleaning up unwanted files ..."
+# Cleaning up unwanted files
 find . -depth -name '*~' -exec rm -rf "{}" \;
 find . -depth -name '#*' -exec rm -rf "{}" \;
 find . -depth -name '.DS_Store' -exec rm "{}" \;
 find . -depth -name 'Thumbs.db' -exec rm "{}" \;
 
-echo "Creating $XPI ..."
 zip -qr9XD "../../$XPI" *
 
-echo "Cleaning up temporary files ..."
 cd ..
 rm -rf build
 cd ..
