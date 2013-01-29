7969f84fcb806aa7439eb4037700d75913ebdd6a
Morac2 <mjk4984@yahoo.com>
2013-01-28 23:39:48 -0500

    Update src/packages/unload.js

--- src/packages/unload.js
+++ src/packages/unload.js
@@ -20,6 +20,7 @@
  * Contributor(s):
  *   Edward Lee <edilee@mozilla.com>
  *   Erik Vold <erikvvold@gmail.com>
+ *   Michael Kraft <morac99-firefox2@yahoo.com>
  *
  * Alternatively, the contents of this file may be used under the terms of
  * either the GNU General Public License Version 2 or later (the "GPL"), or
